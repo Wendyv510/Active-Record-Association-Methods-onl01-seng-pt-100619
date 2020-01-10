804896e45bab14a1f6260d931dae4475ece1ad4d
Wendy Harris <wendyv510@hotmail.com>
2020-01-10 22:23:10 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,7 +15,7 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     # return an array of strings containing every musician's name
    artists = self.artists.map do |artist|
-    artist.names
+    artists.names
     end 
   end
 end
