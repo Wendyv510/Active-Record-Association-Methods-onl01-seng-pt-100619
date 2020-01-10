c5a8e17a5039632e7112f6f955aa773527d92bf9
Wendy Harris <wendyv510@hotmail.com>
2020-01-10 21:59:52 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,6 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
   
-  def get_first_song 
-    self.songs
-  end 
   
   def get_first_song 
     self.songs.first
